1ffab5cea188f5718ea78f204b7c517982779ea8
Anupam Maurya <anupammaurya6767@gmail.com>
2024-02-29 08:47:32 +0530

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='api_gpt4',
-    version='1.0.0',
+    version='1.0.1',
     description='The GPT-4 API is an interface for interacting with the powerful GPT-4 language model. It allows you to generate natural language text, perform question-answering tasks, and more. Whether you\'re building chatbots, content generators, or creative writing tools, GPT-4 has got you covered!',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -19,6 +19,7 @@ setup(
     python_requires='>=3.11',  # Updated version specifier
     install_requires=[
         'selenium==4.18.1',
-        'beautifulsoup4==4.12.2'
+        'beautifulsoup4==4.12.2',
+        'colorama==0.4.6'
     ],
 )
