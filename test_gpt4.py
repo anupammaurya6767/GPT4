7d61ecada6219f4f1a94be9ca8529357dac0e673
Anupam Maurya <anupammaurya6767@gmail.com>
2024-02-29 10:09:51 +0530

    Update test_gpt4.py

--- test_gpt4.py
+++ test_gpt4.py
@@ -12,8 +12,14 @@ class TestGpt4(unittest.TestCase):
     def test_ask_question(self):
         question = 'Test question'
         self.ap.login()
-        self.ap.ask_question(question,10)
+        self.ap.ask_question(question,20)
         response = self.ap.get_response()
+        pass
+    
+    def test_design(self):
+        question = 'A cow'
+        self.ap.login()
+        self.ap.design(question)
         pass
 
     def tearDown(self):
