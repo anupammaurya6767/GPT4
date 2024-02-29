e721eef9dc6110856da891fe53ff07b20b63e9e0
Anupam Maurya <anupammaurya6767@gmail.com>
2024-02-29 17:40:35 +0530

    Update test_gpt4.py

--- test_gpt4.py
+++ test_gpt4.py
@@ -1,9 +1,22 @@
 import unittest
+import os
 from api.gpt4 import GPT4
 
 class TestGpt4(unittest.TestCase):
     def setUp(self):
-        self.ap = GPT4(config_file='config.ini')
+        # Read username and password from environment variables
+        username = os.environ.get('USERNAME')
+        password = os.environ.get('PASSWORD')
+        
+        # Write username and password to a temporary config file
+        temp_config_path = 'temp_config.ini'
+        with open(temp_config_path, 'w') as config_file:
+            config_file.write("[CREDENTIALS]\n")
+            config_file.write(f"username = {username}\n")
+            config_file.write(f"password = {password}\n")
+
+        # Initialize GPT4 with the temporary config file
+        self.ap = GPT4(config_file=temp_config_path)
 
     def test_login(self):
         self.ap.login()
@@ -23,6 +36,7 @@ class TestGpt4(unittest.TestCase):
         pass
 
     def tearDown(self):
+        os.remove('temp_config.ini')
         self.ap.close()
 
 if __name__ == '__main__':
